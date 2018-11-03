cf506cf3f00c93873e9e3a71ad8341abbe4eb808
Zahid <Zahid.SE@Live.com>
2018-11-04 00:43:19 +0100

    Re-factored method signature and made static to allow access from static methods

--- src/main/java/org/right_brothers/Start.java
+++ src/main/java/org/right_brothers/Start.java
@@ -11,9 +11,8 @@ public class Start {
         agents.add("test:org.right_brothers.agents.DummyAgent");
         agents.add("seller1:org.right_brothers.agents.OrderProcessingAgent");
 
-        Start someObjectName = new Start();
         for (int i = 1; i < 2 ; i++) {
-            agents.add(someObjectName.getBuyerAgentInitializationString(i));
+            agents.add(getBuyerAgentInitializationString(i));
         }
 
         List<String> cmd = new Vector<>();
@@ -26,7 +25,7 @@ public class Start {
         cmd.add(sb.toString());
         jade.Boot.main(cmd.toArray(new String[cmd.size()]));
     }
-    private String getBuyerAgentInitializationString(int buyerID){
+    private static String getBuyerAgentInitializationString(int buyerID){
         String order = "{\"order_date\": { \"day\": 1, \"hour\": 0 }, \"guid\": \"order-001\", \"products\": { \"Bagel\": 10, \"Donut\": 1, \"Berliner\": 8, \"Muffin\": 5, \"Bread\": 0 }, \"customer_id\": \"customer-001\", \"delivery_date\": { \"day\": 2, \"hour\": 20 } }";
         String buyerString = "buyer" + buyerID + ":org.right_brothers.agents.BakeryCustomerAgent(" + order + ")";
         return buyerString;
