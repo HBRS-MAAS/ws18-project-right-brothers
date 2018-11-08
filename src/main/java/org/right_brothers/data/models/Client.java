2ac0531fe665587d514f736a2a9c48502488c100
Zahid <Zahid.SE@Live.com>
2018-11-09 00:36:59 +0100

    Added Client class with primary properties

---
+++ src/main/java/org/right_brothers/data/models/Client.java
@@ -0,0 +1,34 @@
+package org.right_brothers.data.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Client {
+	private String guid;
+	private int type;
+	private String name;
+
+	public String getGuid() {
+		return guid;
+	}
+
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
