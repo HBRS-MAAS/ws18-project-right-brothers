657c0d5c8b32cbe99efe281ae7c2e70aeaa7538c
Zahid <Zahid.SE@Live.com>
2018-11-09 01:13:04 +0100

    Updated generic JSON parser because type of the deserialized object is required to be static

--- src/main/java/org/right_brothers/agents/utils/InputParser.java
+++ src/main/java/org/right_brothers/agents/utils/InputParser.java
@@ -8,16 +8,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class InputParser<T> {
 	private String filePath;
+	private TypeReference<?> type;
 	
-	public InputParser(String filePath) {
+	public InputParser(String filePath, TypeReference<?> type) {
 		this.filePath = filePath;
+		this.type = type;
 	}
 	public T parse() {
 		InputStream inputStream = this.getClass().getResourceAsStream(filePath);
 		if ( inputStream != null ) {
 			ObjectMapper mapper = new ObjectMapper();
 			try {
-				T data = mapper.readValue(inputStream, new TypeReference<T>(){});
+				T data = mapper.readValue(inputStream, type);
 				return data;
 			} catch(IOException e) {
 				e.printStackTrace();
