4a849735e74b1a5ad9f241002fe6f8cdd580fc99
Zahid <Zahid.SE@Live.com>
2018-11-11 01:19:30 +0100

    Removed redundant field definition from UnbakedProduct message

--- src/main/java/org/right_brothers/data/messages/UnbakedProduct.java
+++ src/main/java/org/right_brothers/data/messages/UnbakedProduct.java
@@ -1,13 +1,3 @@
 package org.right_brothers.data.messages;
 
-public class UnbakedProduct {
-	private String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-}
+public class UnbakedProduct extends CoordinatorMessage{}
