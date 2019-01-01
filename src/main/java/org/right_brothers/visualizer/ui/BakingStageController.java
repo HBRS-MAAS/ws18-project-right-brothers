53f2889206d797942cca59d84eb80b651d0087eb
Zahid <Zahid.SE@Live.com>
2019-01-01 17:48:14 +0100

    visualizer: added messages from proofer to visualizer

--- src/main/java/org/right_brothers/visualizer/ui/BakingStageController.java
+++ src/main/java/org/right_brothers/visualizer/ui/BakingStageController.java
@@ -2,8 +2,16 @@ package org.right_brothers.visualizer.ui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import org.maas.utils.JsonConverter;
+import org.right_brothers.data.messages.UnbakedProductMessage;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,21 +24,55 @@ public class BakingStageController implements Initializable, StageController {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		try {
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/right_brothers/BakingCard.fxml"));
-			Parent bakingCard = fxmlLoader.load();
-			container.getChildren().add(bakingCard);
-			
-			BakingCardController controller =  fxmlLoader.getController();
-			controller.setText("Berliner", "order-001(10), order-002(20)");
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/right_brothers/BakingCard.fxml"));
+//			Parent bakingCard = fxmlLoader.load();
+//			container.getChildren().add(bakingCard);
+//			
+//			BakingCardController controller =  fxmlLoader.getController();
+//			controller.setText("Berliner", "order-001(10), order-002(20)");
+//		} catch(IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	@Override
 	public void updateStage(String messageType, String message) {
-		// TODO Auto-generated method stub
+		// TODO - Use Regex instead of starts with for cooling rack message
+		if(messageType.equalsIgnoreCase("baking-request") || messageType.startsWith("cooled-product-")) {
+			UnbakedProductMessage unbakedProductMessage = JsonConverter
+					.getInstance(message, new TypeReference<UnbakedProductMessage>() {});
+			
+			if(messageType.equalsIgnoreCase("baking-request")) {
+				addCard(unbakedProductMessage);
+			}
+		}
+	}
+	
+	private void addCard(UnbakedProductMessage message) {
+		List<String> orders = new ArrayList<String>();
+		
+		for(int i=0; i< message.getGuids().size(); i++) {
+			orders.add(String.format("%s(%s)", 
+					message.getGuids().get(i), message.getProductQuantities().get(i)));
+		}
+		
+		Platform.runLater(
+				  () -> {
+					  try {
+							FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/right_brothers/BakingCard.fxml"));
+							Parent bakingCard = fxmlLoader.load();
+							container.getChildren().add(0, bakingCard);
+							
+							BakingCardController controller =  fxmlLoader.getController();
+							controller.setText(message.getProductType(), String.join(" ", orders));
+						} catch(IOException e) {
+							e.printStackTrace();
+						}
+				  }
+				);
+		
+		
 	}
 
 }
