a6086edf660dedb3efd2f3c41bfe7d353341e776
Zahid <Zahid.SE@Live.com>
2019-01-02 00:08:53 +0100

    visualizer: Extend visualizer from base agent

--- src/main/java/org/right_brothers/agents/VisualizationAgent.java
+++ src/main/java/org/right_brothers/agents/VisualizationAgent.java
@@ -1,37 +1,72 @@
 package org.right_brothers.agents;
 
+import java.util.ArrayList;
+
+import org.maas.agents.BaseAgent;
+import org.maas.agents.TimeKeeper;
+import org.maas.objects.ProcessedProduct;
+import org.maas.utils.Time;
 import org.right_brothers.visualizer.ui.Visualizer;
 import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 
 @SuppressWarnings("serial")
-public class VisualizationAgent extends Agent {
-
+public class VisualizationAgent extends BaseAgent {
+	private String scenarioDirectory;
+	private Time endTime;
+	
     private Visualizer guiWindow;
 
 	protected void setup() {
-	// Printout a welcome message
+		super.setup();
+		
+		Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            scenarioDirectory = (String) args[0];
+            String endTimeString = (String) args[1];
+            endTime = new Time(endTimeString);
+        } else {
+            scenarioDirectory = "small";
+            endTime = new Time(1,12,0);
+        }
+        
+		// Printout a welcome message
 		System.out.println("Hello! Visualization-agent "+getAID().getName()+" is ready.");
+		
+		this.register("board-visualisation", "board-visualisation-agent");
 
         // launch the gui window in another thread
-        new Thread() {
+        Thread thread = new Thread() {
             @Override
             public void run() {
                 Visualizer.run(new String[] {});
             }
-        }.start();
-        guiWindow = Visualizer.waitForInstance();
+        };
+    	thread.start();
+        
+    	guiWindow = Visualizer.waitForInstance();
 		addBehaviour(new MessageServer());
 	}
+	
+    @Override
+    protected void stepAction(){
+        if (baseAgent.getCurrentTime().lessThan(this.endTime)) {
+        	baseAgent.finished();
+        }
+    }
+    
 	protected void takeDown() {
+		this.deRegister();
 		System.out.println(getAID().getLocalName() + ": Terminating.");
 	}
 
     private class MessageServer extends CyclicBehaviour {
         public void action() {
-            ACLMessage msg = myAgent.receive();
+        	MessageTemplate mt = MessageTemplate.not(MessageTemplate.MatchPerformative(TimeKeeper.BROADCAST_TIMESTEP_PERFORMATIVE));
+            ACLMessage msg = myAgent.receive(mt);
             if (msg != null) {
                 guiWindow.updateBoard(msg.getConversationId().toLowerCase(), msg.getContent());
             }
