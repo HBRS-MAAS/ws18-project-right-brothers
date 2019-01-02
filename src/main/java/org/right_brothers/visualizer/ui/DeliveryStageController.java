5095dca8bc59aa4cda43443b324b3741f9c9bf37
Zahid <Zahid.SE@Live.com>
2019-01-02 14:48:41 +0100

    visualizer: show total message count in a stage

--- src/main/java/org/right_brothers/visualizer/ui/DeliveryStageController.java
+++ src/main/java/org/right_brothers/visualizer/ui/DeliveryStageController.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class DeliveryStageController implements Initializable, StageController {
 	@FXML
 	private VBox container;
+	
+	@FXML
+	private Label cardCount;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -30,7 +35,10 @@ public class DeliveryStageController implements Initializable, StageController {
 
 	@Override
 	public void updateStage(String messageType, String message) {
-		// TODO Auto-generated method stub
-		
+		Platform.runLater(
+				  () -> {
+					  cardCount.setText(Integer.toString(container.getChildren().size()));
+				  }
+				);
 	}
 }
