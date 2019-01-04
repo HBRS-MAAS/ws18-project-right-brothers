0d4b2a58074d7f2aaf1a10bd9ffe99ea2f21ef95
Zahid <Zahid.SE@Live.com>
2019-01-04 15:04:34 +0100

    visualizer: changed fade transition to highlight the newly added cards

--- src/main/java/org/right_brothers/visualizer/ui/StageController.java
+++ src/main/java/org/right_brothers/visualizer/ui/StageController.java
@@ -16,9 +16,9 @@ public abstract class StageController implements ScenarioAware{
 	
 	protected void highlightCard(Node node) {
 		FadeTransition ft = new FadeTransition(Duration.millis(200), node);
-		ft.setFromValue(1.0);
-		ft.setToValue(0.5);
-		ft.setCycleCount(2);
+		ft.setFromValue(0.7);
+		ft.setToValue(1.0);
+		ft.setCycleCount(1);
 		ft.setAutoReverse(true);
  
 		ft.play();
