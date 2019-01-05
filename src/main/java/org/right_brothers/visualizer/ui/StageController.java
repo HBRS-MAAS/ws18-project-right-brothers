61c80aaacdfb6b9f5e9cd603ae3e068aa55f2592
Zahid <Zahid.SE@Live.com>
2019-01-05 14:26:24 +0100

    visualizer: added layout to display current time

--- src/main/java/org/right_brothers/visualizer/ui/StageController.java
+++ src/main/java/org/right_brothers/visualizer/ui/StageController.java
@@ -16,7 +16,7 @@ public abstract class StageController implements ScenarioAware{
 	
 	protected void highlightCard(Node node) {
 		FadeTransition ft = new FadeTransition(Duration.millis(200), node);
-		ft.setFromValue(0.7);
+		ft.setFromValue(0.5);
 		ft.setToValue(1.0);
 		ft.setCycleCount(1);
 		ft.setAutoReverse(true);
