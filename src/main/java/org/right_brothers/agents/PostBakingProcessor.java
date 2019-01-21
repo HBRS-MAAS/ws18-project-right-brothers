2998b117c4af7742709b30d6f96b96ff1182bee4
DRealArun <gitecarp@gmail.com>
2019-01-21 19:12:16 +0100

    Solving codacy issues

--- src/main/java/org/right_brothers/agents/PostBakingProcessor.java
+++ src/main/java/org/right_brothers/agents/PostBakingProcessor.java
@@ -6,8 +6,6 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 import java.util.*;
-import java.io.IOException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import org.maas.agents.BaseAgent;
